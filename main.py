e01154e020935641eae3446e594622dd149166fc
alekseeve <alekseeve@stu.otc.school.nz>
2022-08-08 15:20:03 +1200

    new

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-print("hello")
+import random
+def MenuP():
+
+    answer = input("Type in the number corresponding to the desired generation style.\n"
+                   "1. Magic 8 Ball\n2. Dice\n3. Random Number\n4. "
+                   "Passwords Generation\n5. Names Generation\n6. Exit\n")
+    if answer == "1":
+        pass
+
+    elif answer == "2":
+        pass
+
+    elif answer == "3":
+        pass
+
+    elif answer == "4":
+        pass
+
+    elif answer == "5":
+        pass
+
+    elif answer == "6":
+        pass
+
+    else:
+        answer = input("Please input a valid option!")
+
+MenuP()
 
