4f5e70e16a26ec6c947beb218f08c83c1b896450
alekseeve <alekseeve@stu.otc.school.nz>
2022-08-10 22:58:55 -0230

    new

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 def color(r, g, b, text):
     return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
 
-#Menu Popup: User can choose style of generation.
+#Menu Choice: User can choose style of generation.
 def MenuP():
     while True:
         print(color(0,200,255,"Type in the number corresponding to the desired generation style."))
@@ -38,23 +38,26 @@ def MenuP():
 def EndP():
     exit()
 
-#8-Ball Popup: User can shake ball, Menu or back.
+#8-Ball Choice: User can shake ball, Menu or back.
 def Magic8Ball():
+    balldict = ["It is certain.","It is decidedly so.","Without a doubt.","Yes definitely.",
+                "You may rely on it.","As I see it, yes.","Most likely.","Outlook good.","Yes.",
+                "Signs point to yes.","Im legally not allowed to tell you.","That is a very stupid question.",
+                "You cant face the truth.","Are you drunk?","Is this even a question?","No!",
+                "It's quite obvious the answer is no.","I dont think so","Nah","What? absolutly not."]
     while True:
-        print(color(0,200,255,"Please choose an option in number form"))
-        answer = input("1. Shake 8-Ball\n2. Back to Menu\n3. Exit Program\n\n\n\n\n")
+        print(color(0,200,255,"Type '1' to get back to menu or '2' to exit the program."))
+        print(color(150,80,230,"Note that the 8-Ball will resond to No/Yes answers only.\n"
+                               "Type your questions below."))
+        answer = input("1. Back to Menu\n" + color(200,10,10,"2. Exit Program\n\n\n\n\n\n"))
         if answer == "1":
-            pass
-
-        elif answer == "2":
             MenuP()
 
-        elif answer == "3":
-            EndP()
+        elif answer == "2":
+            ExitP()
 
         else:
-            print("-----------------------")
-            print(color(255,165,0,"Please input a valid option!"))
-            print("-----------------------")
+            ballgenchoice = random.choice(balldict)
+            print(ballgenchoice)
 MenuP()
 
